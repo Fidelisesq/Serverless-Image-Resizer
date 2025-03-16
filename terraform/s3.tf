35ad32f68266a00f8d41b3cab333c3b9931cdf48
Fidelis Ikoroje <fidelis.ikoroje@gmail.com>
2025-03-16 17:15:25 +0000

    Fill

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -67,20 +67,5 @@ resource "aws_s3_bucket_public_access_block" "lambda_code_bucket_block" {
 }
 
 
-# Policy: Allow Terraform IAM Role & Lambda to Read Objects
-resource "aws_s3_bucket_policy" "lambda_code_policy" {
-  bucket = aws_s3_bucket.lambda_code_bucket.id
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect    = "Allow",
-        Principal = "*",
-        Action    = ["s3:GetObject", "s3:PutObject"],
-        Resource  = "${aws_s3_bucket.lambda_code_bucket.arn}/*"
-      }
-    ]
-  })
-}
 
 
