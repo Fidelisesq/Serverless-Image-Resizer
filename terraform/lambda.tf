09898dfd7027494496fc7ada2e6b543ee5256761
Fidelis Ikoroje <fidelis.ikoroje@gmail.com>
2025-03-16 16:38:11 +0000

    Retest

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -47,29 +47,7 @@ resource "null_resource" "lambda_zip" {
   }
 }
 
-# Lambda Execution Role
-resource "aws_iam_role" "lambda_exec_role" {
-  name = "lambda-exec-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
 
-# Lambda Execution Role Policy
-resource "aws_iam_policy_attachment" "lambda_policy_attachment" {
-  name       = "lambda-policy-attachment"
-  roles      = [aws_iam_role.lambda_exec_role.name]
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
 
 # Lambda Functions (Presign, Resize, List, Delete)
 resource "aws_lambda_function" "presign" {
