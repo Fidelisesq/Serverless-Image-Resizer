3c262ab8c193d66b42cb7b83fbe4aa4b247452e0
Fidelis Ikoroje <fidelis.ikoroje@gmail.com>
2025-03-16 16:05:53 +0000

    IAM

---
+++ terraform/iam.tf
@@ -0,0 +1,38 @@
+# IAM Role for Lambda Execution
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "lambda_exec_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action    = "sts:AssumeRole",
+      Effect    = "Allow",
+      Principal = { Service = "lambda.amazonaws.com" }
+    }]
+  })
+}
+
+# IAM Policy: Allow Lambda to Access S3 (Original, Resized, and Lambda Code Buckets)
+resource "aws_iam_policy" "lambda_s3_access" {
+  name        = "LambdaS3Access"
+  description = "Allows Lambda to read/write S3 objects"
+  policy      = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect    = "Allow",
+        Action    = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"],
+        Resource  = [
+          "${aws_s3_bucket.original.arn}/*",
+          "${aws_s3_bucket.resized.arn}/*",
+          "${aws_s3_bucket.lambda_code.arn}/*"
+        ]
+      }
+    ]
+  })
+}
+
+# Attach IAM Policy to Lambda Execution Role
+resource "aws_iam_role_policy_attachment" "attach_lambda_s3_policy" {
+  policy_arn = aws_iam_policy.lambda_s3_access.arn
+  role       = aws_iam_role.lambda_exec_role.name
+}
