48afe7dfbcaae66e003d52e3866fe3417ade042d
Fidelis Ikoroje <fidelis.ikoroje@gmail.com>
2025-03-16 18:23:49 +0000

    IAM policy

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -65,15 +65,17 @@ resource "aws_s3_bucket_versioning" "lambda_code_bucket_versioning" {
   }
 }
 
+/*
 # Block all public access for the Lambda code bucket
 resource "aws_s3_bucket_public_access_block" "lambda_code_bucket_block" {
   bucket = aws_s3_bucket.lambda_code_bucket.id
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
+*/
 
 
 
