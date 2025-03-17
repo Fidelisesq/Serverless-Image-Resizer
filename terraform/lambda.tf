1814b934e028e975df24d9221fb27adb4c32b58c
Fidelis Ikoroje <fidelis.ikoroje@gmail.com>
2025-03-17 16:03:31 +0000

    Commit

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,19 +1,19 @@
-# Zip Lambda code and upload to S3
+# Create Lambda function zip files
 resource "null_resource" "lambda_zip" {
   provisioner "local-exec" {
-    command = "cd ${path.module}/../lambda/presign && zip -r ../presign.zip ."
+    command = "cd ${path.module}/../lambda/presign && zip -r ${path.module}/../presign.zip ."
   }
 
   provisioner "local-exec" {
-    command = "cd ${path.module}/../lambda/resize && zip -r ../resize.zip ."
+    command = "cd ${path.module}/../lambda/resize && zip -r ${path.module}/../resize.zip ."
   }
 
   provisioner "local-exec" {
-    command = "cd ${path.module}/../lambda/list && zip -r ../list.zip ."
+    command = "cd ${path.module}/../lambda/list && zip -r ${path.module}/../list.zip ."
   }
 
   provisioner "local-exec" {
-    command = "cd ${path.module}/../lambda/delete && zip -r ../delete.zip ."
+    command = "cd ${path.module}/../lambda/delete && zip -r ${path.module}/../delete.zip ."
   }
 
   triggers = {
@@ -21,45 +21,46 @@ resource "null_resource" "lambda_zip" {
   }
 }
 
+# Upload zipped Lambda code to S3
 resource "aws_s3_object" "lambda_presign" {
   depends_on = [null_resource.lambda_zip]
-  bucket = aws_s3_bucket.lambda_code_bucket.bucket
-  key    = "presign.zip"
-  source = "${path.module}/../lambda/presign.zip"
+  bucket     = aws_s3_bucket.lambda_code_bucket.bucket
+  key        = "presign.zip"
+  source     = "${path.module}/../presign.zip"
 }
 
 resource "aws_s3_object" "lambda_resize" {
   depends_on = [null_resource.lambda_zip]
-  bucket = aws_s3_bucket.lambda_code_bucket.bucket
-  key    = "resize.zip"
-  source = "${path.module}/../lambda/resize.zip"
+  bucket     = aws_s3_bucket.lambda_code_bucket.bucket
+  key        = "resize.zip"
+  source     = "${path.module}/../resize.zip"
 }
 
 resource "aws_s3_object" "lambda_list" {
   depends_on = [null_resource.lambda_zip]
-  bucket = aws_s3_bucket.lambda_code_bucket.bucket
-  key    = "list.zip"
-  source = "${path.module}/../lambda/list.zip"
+  bucket     = aws_s3_bucket.lambda_code_bucket.bucket
+  key        = "list.zip"
+  source     = "${path.module}/../list.zip"
 }
 
 resource "aws_s3_object" "lambda_delete" {
   depends_on = [null_resource.lambda_zip]
-  bucket = aws_s3_bucket.lambda_code_bucket.bucket
-  key    = "delete.zip"
-  source = "${path.module}/../lambda/delete.zip"
+  bucket     = aws_s3_bucket.lambda_code_bucket.bucket
+  key        = "delete.zip"
+  source     = "${path.module}/../delete.zip"
 }
 
-# Lambda Functions with source_code_hash for automatic updates
+# Lambda Function for Presign
 resource "aws_lambda_function" "presign" {
   depends_on       = [aws_s3_object.lambda_presign]
   function_name    = "presign"
-  handler         = "index.handler"
-  runtime         = "nodejs18.x"
-  role            = aws_iam_role.lambda_exec_role.arn
-  s3_bucket       = aws_s3_bucket.lambda_code_bucket.bucket
-  s3_key          = "presign.zip"
-  timeout         = 10
-  source_code_hash = filebase64sha256("${path.module}/../lambda/presign.zip")
+  handler          = "index.handler"
+  runtime          = "nodejs18.x"
+  role             = aws_iam_role.lambda_exec_role.arn
+  s3_bucket        = aws_s3_bucket.lambda_code_bucket.bucket
+  s3_key           = "presign.zip"
+  timeout          = 10
+  source_code_hash = filebase64sha256("${path.module}/../presign.zip")
 
   environment {
     variables = {
@@ -68,16 +69,17 @@ resource "aws_lambda_function" "presign" {
   }
 }
 
+# Lambda Function for Resize
 resource "aws_lambda_function" "resize" {
   depends_on       = [aws_s3_object.lambda_resize]
   function_name    = "resize"
-  handler         = "index.handler"
-  runtime         = "nodejs18.x"
-  role            = aws_iam_role.lambda_exec_role.arn
-  s3_bucket       = aws_s3_bucket.lambda_code_bucket.bucket
-  s3_key          = "resize.zip"
-  timeout         = 10
-  source_code_hash = filebase64sha256("${path.module}/../lambda/resize.zip")
+  handler          = "index.handler"
+  runtime          = "nodejs18.x"
+  role             = aws_iam_role.lambda_exec_role.arn
+  s3_bucket        = aws_s3_bucket.lambda_code_bucket.bucket
+  s3_key           = "resize.zip"
+  timeout          = 10
+  source_code_hash = filebase64sha256("${path.module}/../resize.zip")
 
   environment {
     variables = {
@@ -87,16 +89,17 @@ resource "aws_lambda_function" "resize" {
   }
 }
 
+# Lambda Function for List
 resource "aws_lambda_function" "list" {
   depends_on       = [aws_s3_object.lambda_list]
   function_name    = "list"
-  handler         = "index.handler"
-  runtime         = "nodejs18.x"
-  role            = aws_iam_role.lambda_exec_role.arn
-  s3_bucket       = aws_s3_bucket.lambda_code_bucket.bucket
-  s3_key          = "list.zip"
-  timeout         = 10
-  source_code_hash = filebase64sha256("${path.module}/../lambda/list.zip")
+  handler          = "index.handler"
+  runtime          = "nodejs18.x"
+  role             = aws_iam_role.lambda_exec_role.arn
+  s3_bucket        = aws_s3_bucket.lambda_code_bucket.bucket
+  s3_key           = "list.zip"
+  timeout          = 10
+  source_code_hash = filebase64sha256("${path.module}/../list.zip")
 
   environment {
     variables = {
@@ -105,16 +108,17 @@ resource "aws_lambda_function" "list" {
   }
 }
 
+# Lambda Function for Delete
 resource "aws_lambda_function" "delete" {
   depends_on       = [aws_s3_object.lambda_delete]
   function_name    = "delete"
-  handler         = "index.handler"
-  runtime         = "nodejs18.x"
-  role            = aws_iam_role.lambda_exec_role.arn
-  s3_bucket       = aws_s3_bucket.lambda_code_bucket.bucket
-  s3_key          = "delete.zip"
-  timeout         = 10
-  source_code_hash = filebase64sha256("${path.module}/../lambda/delete.zip")
+  handler          = "index.handler"
+  runtime          = "nodejs18.x"
+  role             = aws_iam_role.lambda_exec_role.arn
+  s3_bucket        = aws_s3_bucket.lambda_code_bucket.bucket
+  s3_key           = "delete.zip"
+  timeout          = 10
+  source_code_hash = filebase64sha256("${path.module}/../delete.zip")
 
   environment {
     variables = {
