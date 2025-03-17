af5b15910a4bfbe785389300d8f59e0e1dfb3168
Fidelis Ikoroje <fidelis.ikoroje@gmail.com>
2025-03-17 16:10:33 +0000

    Change

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,19 +1,19 @@
-# Create Lambda function zip files
+# Create Lambda function zip files in the local environment
 resource "null_resource" "lambda_zip" {
   provisioner "local-exec" {
-    command = "cd ${path.module}/../lambda/presign && zip -r ${path.module}/../presign.zip ."
+    command = "cd ${path.module}/../lambda/presign && zip -r ${path.module}/presign.zip ."
   }
 
   provisioner "local-exec" {
-    command = "cd ${path.module}/../lambda/resize && zip -r ${path.module}/../resize.zip ."
+    command = "cd ${path.module}/../lambda/resize && zip -r ${path.module}/resize.zip ."
   }
 
   provisioner "local-exec" {
-    command = "cd ${path.module}/../lambda/list && zip -r ${path.module}/../list.zip ."
+    command = "cd ${path.module}/../lambda/list && zip -r ${path.module}/list.zip ."
   }
 
   provisioner "local-exec" {
-    command = "cd ${path.module}/../lambda/delete && zip -r ${path.module}/../delete.zip ."
+    command = "cd ${path.module}/../lambda/delete && zip -r ${path.module}/delete.zip ."
   }
 
   triggers = {
@@ -26,28 +26,28 @@ resource "aws_s3_object" "lambda_presign" {
   depends_on = [null_resource.lambda_zip]
   bucket     = aws_s3_bucket.lambda_code_bucket.bucket
   key        = "presign.zip"
-  source     = "${path.module}/../presign.zip"
+  source     = "${path.module}/presign.zip"
 }
 
 resource "aws_s3_object" "lambda_resize" {
   depends_on = [null_resource.lambda_zip]
   bucket     = aws_s3_bucket.lambda_code_bucket.bucket
   key        = "resize.zip"
-  source     = "${path.module}/../resize.zip"
+  source     = "${path.module}/resize.zip"
 }
 
 resource "aws_s3_object" "lambda_list" {
   depends_on = [null_resource.lambda_zip]
   bucket     = aws_s3_bucket.lambda_code_bucket.bucket
   key        = "list.zip"
-  source     = "${path.module}/../list.zip"
+  source     = "${path.module}/list.zip"
 }
 
 resource "aws_s3_object" "lambda_delete" {
   depends_on = [null_resource.lambda_zip]
   bucket     = aws_s3_bucket.lambda_code_bucket.bucket
   key        = "delete.zip"
-  source     = "${path.module}/../delete.zip"
+  source     = "${path.module}/delete.zip"
 }
 
 # Lambda Function for Presign
@@ -60,7 +60,7 @@ resource "aws_lambda_function" "presign" {
   s3_bucket        = aws_s3_bucket.lambda_code_bucket.bucket
   s3_key           = "presign.zip"
   timeout          = 10
-  source_code_hash = filebase64sha256("${path.module}/../presign.zip")
+  source_code_hash = filebase64sha256("${path.module}/presign.zip")
 
   environment {
     variables = {
@@ -79,7 +79,7 @@ resource "aws_lambda_function" "resize" {
   s3_bucket        = aws_s3_bucket.lambda_code_bucket.bucket
   s3_key           = "resize.zip"
   timeout          = 10
-  source_code_hash = filebase64sha256("${path.module}/../resize.zip")
+  source_code_hash = filebase64sha256("${path.module}/resize.zip")
 
   environment {
     variables = {
@@ -99,7 +99,7 @@ resource "aws_lambda_function" "list" {
   s3_bucket        = aws_s3_bucket.lambda_code_bucket.bucket
   s3_key           = "list.zip"
   timeout          = 10
-  source_code_hash = filebase64sha256("${path.module}/../list.zip")
+  source_code_hash = filebase64sha256("${path.module}/list.zip")
 
   environment {
     variables = {
@@ -118,7 +118,7 @@ resource "aws_lambda_function" "delete" {
   s3_bucket        = aws_s3_bucket.lambda_code_bucket.bucket
   s3_key           = "delete.zip"
   timeout          = 10
-  source_code_hash = filebase64sha256("${path.module}/../delete.zip")
+  source_code_hash = filebase64sha256("${path.module}/delete.zip")
 
   environment {
     variables = {
