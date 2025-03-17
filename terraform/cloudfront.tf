2062f09c0d121426794ced39044f7143ef7089b8
Fidelis Ikoroje <fidelis.ikoroje@gmail.com>
2025-03-17 17:29:20 +0000

    Checking

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -36,7 +36,7 @@ resource "aws_cloudfront_distribution" "frontend_distribution" {
   viewer_certificate {
     acm_certificate_arn      = var.acm_certificate_arn
     ssl_support_method       = "sni-only"
-    minimum_protocol_version = "TLSv1.2_2021"
+    minimum_protocol_version = "TLSv1.2_2019"
   }
 
   restrictions {
