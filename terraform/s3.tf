1f36eb8e1d23140acf8807969b609721960a168a
Fidelis Ikoroje <fidelis.ikoroje@gmail.com>
2025-03-18 18:47:13 +0000

    Resized

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -40,7 +40,6 @@ resource "aws_s3_bucket_public_access_block" "resized_block" {
 }
 
 # Attach Policy to Frontend Bucket (For CloudFront & Uploads)
-
 resource "aws_s3_bucket_policy" "frontend_policy" {
   bucket = aws_s3_bucket.frontend.id  # Attach policy to frontend (CloudFront)
 
@@ -92,7 +91,7 @@ resource "aws_s3_bucket_policy" "frontend_policy" {
   })
 }
 
-# Fix: Allow CloudFront to Serve Processed Images from "resized"
+#Allow CloudFront to Serve Processed Images from "resized"
 resource "aws_s3_bucket_policy" "resized_policy" {
   bucket = aws_s3_bucket.resized.id
 
@@ -117,31 +116,6 @@ resource "aws_s3_bucket_policy" "resized_policy" {
   })
 }
 
-
-# Allow CloudFront to Serve Processed Images from "resized"
-resource "aws_s3_bucket_policy" "resized_policy" {
-  bucket = aws_s3_bucket.resized.id
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Sid    = "AllowCloudFrontAccessResized",
-        Effect = "Allow",
-        Principal = {
-          Service = "cloudfront.amazonaws.com"
-        },
-        Action   = "s3:GetObject",
-        Resource = "${aws_s3_bucket.resized.arn}/*",
-        Condition = {
-          StringEquals = {
-            "AWS:SourceArn" = "arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/${aws_cloudfront_distribution.frontend_distribution.id}"
-          }
-        }
-      }
-    ]
-  })
-}
 
 # Enable CORS for Uploads & Image Access (Original Bucket)
 resource "aws_s3_bucket_cors_configuration" "original_cors" {
