8f9afccb08f447cfc79aa3eba60a88c5a09cd15b
Fidelis Ikoroje <fidelis.ikoroje@gmail.com>
2025-03-24 08:15:25 +0000

    Source

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -73,11 +73,11 @@ resource "aws_s3_bucket_policy" "original_bucket_policy" {
       },
       Action    = "s3:GetObject",
       Resource  = "${aws_s3_bucket.original.arn}/*",
-      /*Condition = {
+      Condition = {
         StringEquals = {
-          "AWS:SourceArn" = aws_cloudfront_distribution.frontend_distribution.arn
+           "AWS:SourceAccount" = data.aws_caller_identity.current.account_id
         }
-      }*/
+      }
     }]
   })
 }
