f706d1ed0e5adb6c2c56981b74cafd350a7e3bb6
Fidelis Ikoroje <fidelis.ikoroje@gmail.com>
2025-04-08 21:11:03 +0100

    Deploy

--- frontend/app.js
+++ frontend/app.js
@@ -1,5 +1,5 @@
 (function ($) {
-    const apiGatewayBaseUrl = "https://p52e0ymtx8.execute-api.us-east-1.amazonaws.com/prod";
+    const apiGatewayBaseUrl = "https://45pgabb0zj.execute-api.us-east-1.amazonaws.com/prod";
     const cloudfrontBaseUrl = "https://image-resizer.fozdigitalz.com";
 
     const defaultUrls = {
