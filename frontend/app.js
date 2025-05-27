462dc27683641056b151aa752591630acf17d6da
Fidelis Ikoroje <fidelis.ikoroje@gmail.com>
2025-05-27 20:14:40 +0100

    destroy

--- frontend/app.js
+++ frontend/app.js
@@ -1,5 +1,5 @@
 (function ($) {
-    const apiGatewayBaseUrl = "https://bl19s75389.execute-api.us-east-1.amazonaws.com/prod";
+    const apiGatewayBaseUrl = "https://bl19s75389.execute-api.us-east-1.amazonaws.com/";
     const cloudfrontBaseUrl = "https://image-resizer.fozdigitalz.com";
 
     const resizeOptionsGrouped = [
